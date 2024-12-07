e45076f34e0db850eac23fd18c57053a33a8ed86
kato83 <katomeganeb83@gmail.com>
2024-12-07 18:59:25 +0900

    cookieの値を取り扱う関数 getCookieMap を作成

---
+++ frontend/src/utils/cookie.js
@@ -0,0 +1,8 @@
+/**
+ * 全てのクッキーを取得する
+ * @returns cookieのセット
+ */
+export const getCookieMap = () => new Map(document.cookie
+  .split(';')
+  .map(data => data.split('=')
+    .map(data => decodeURIComponent(data))));
