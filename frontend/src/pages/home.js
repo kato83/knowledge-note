72219a6512b8cff2aed731662234a6b5486a2693
kato83 <katomeganeb83@gmail.com>
2024-12-21 20:21:48 +0900

    sample.html を home.html に置き換えて、モックも付け加える

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -2,7 +2,7 @@ import mustache from 'mustache';
 // Viteのルールとして、インポートする対象のファイルをそのまま取得するためには相対パスの末尾に"?raw"を付与する必要がある
 // この場合、テンプレートのHTMLファイルをそのまま取得したいので"?raw"を末尾に付与している
 // 参照: https://ja.vite.dev/guide/assets.html#importing-asset-as-string
-import html from '../templates/sample.html?raw';
+import html from '../templates/home.html?raw';
 
 export const home = () => {
   const app = document.querySelector('#app');
